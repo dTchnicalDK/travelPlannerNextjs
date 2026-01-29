983bf9977430b3c8a7982a55e1ff39fcba77944a
dtechnicaldk <dtechnicaldk@gmail.com>
2026-01-29 21:03:15 +0530

    x button and max size tweaked

--- src/components/orderSummary/OrderSummary.tsx
+++ src/components/orderSummary/OrderSummary.tsx
@@ -1,4 +1,4 @@
-import { Cross, CrosshairIcon } from "lucide-react";
+import { Cross, CrosshairIcon, X } from "lucide-react";
 import React from "react";
 import { SelectSeparator } from "../ui/select";
 import { Separator } from "@radix-ui/react-separator";
@@ -7,10 +7,11 @@ import OrderCard from "./OrderCard";
 const OrderSummary = () => {
   return (
     //container
-    <div className="min-w-screen max-w-100 bg-white text-black flex flex-col  ">
+    <div className=" max-w-100 bg-white text-black flex flex-col  ">
       {/* //title section */}
       <div className="bg-sky-600 text-white w-full h-15 flex justify-start items-center gap-7 px-10">
-        <CrosshairIcon />
+        {/* <CrosshairIcon /> */}
+        <X />
         <p className="text-2xl font-bold">checkout</p>
       </div>
       {/* //progress */}
